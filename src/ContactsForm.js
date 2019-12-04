994e445b09fc5a17b9180edc93b0f8d4f40020d1
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2019-12-04 19:55:35 +0100

    i dont even know if this thing is working or not :'(

--- src/ContactsForm.js
+++ src/ContactsForm.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import './App.css';
 import WorkExperience from "./WorkXPForm";
 import ReactDOM from "react-dom";
+import {connect} from 'react-redux'
+import {Field, reduxForm, formValueSelector} from 'redux-form'
 
 class ContactsForm extends React.Component {
     render() {
         return (
-            <div>
+            <form onSubmit={next}>
                 <div className="input_field">
-                    <label htmlFor={"nameInputField"}>Name:</label><input type={"text"} onChange={handleNameChange}
-                                                                          id={"nameInputField"} value={this.state.name}/>
+                    <label htmlFor={"nameInputField"}>Name:</label><input type={"text"} id={"nameInputField"}/>
                 </div>
                 <div className="input_field">
                     <label htmlFor={"telInputField"}>Phone:</label><input type={"tel"} id={"telInputField"}/>
@@ -20,20 +20,15 @@ class ContactsForm extends React.Component {
                 <div className="input_field">
                     <label htmlFor={"addressInputField"}>Address:</label><input type={"text"} id={"addressInputField"}/>
                 </div>
-                <button type={"button"} onClick={next}>Next</button>
-            </div>
+                <button type={"submit"}>Next</button>
+            </form>
         )
     }
 }
 
-function handleNameChange(e) {
-    this.setState({name: e.target.value});
-}
-
 function next(e) {
     e.preventDefault();
-    //  let name = document.getElementById("nameInputField");
-    console.log("Tha name is: " + this.state.name);
+    console.log("Tha name is this: ");
     ReactDOM.render(<WorkExperience/>, document.getElementById("root"));
 }
 
